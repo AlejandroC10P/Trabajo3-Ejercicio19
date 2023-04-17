9bef5a48b2e7022e7ebe3dd1370f15b7c040892c
Alejandro Castro <111087081+AlejandroC10P@users.noreply.github.com>
2023-04-17 16:45:13 -0500

    Add files via upload

---
+++ Ejercicio19.java
@@ -0,0 +1,11 @@
+package Capitulo_3;
+
+
+public class Ejercicio19 {
+
+    
+    public static void main(String[] args) {
+        InterfazEjercicio19 form = new InterfazEjercicio19();
+        form.setVisible(true);
+    }
+}
